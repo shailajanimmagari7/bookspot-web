83c7c12947593b531fe68323cc5d79cb7d702d8b
shailajanimmagari7 <shailaja.nimmagari@everest.engineering>
2024-12-12 11:07:44 +0530

    Modify test for app component

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders the login page and check the login button', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const loginButton = screen.getByRole('button', { name: /login/i });
+  expect(loginButton).toBeInTheDocument(); 
 });
