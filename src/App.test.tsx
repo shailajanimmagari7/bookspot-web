a0eaf32463f341089362e70f417f70c767d4c44d
shailajanimmagari7 <shailaja.nimmagari@everest.engineering>
2024-12-13 12:00:28 +0530

    Update the test for app component

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders the login page and check the login button', () => {
+test('App renders LoginPage component', () => {
   render(<App />);
-  const loginButton = screen.getByRole('button', { name: /login/i });
-  expect(loginButton).toBeInTheDocument(); 
+  const loginPageHeading = screen.getByRole("heading", { name: /login/i }); 
+  expect(loginPageHeading).toBeInTheDocument();
 });
